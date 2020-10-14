a3a746a5654e95215965321ce92e1f650d798406
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-10-14 15:24:25 -0500

    prieros endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('main')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('holi')
+  getHoli(): any {
+    return this.appService.hola();
+  }
+
+  @Get('palabra/:palabra')
+  getParametro(@Param('palabra') texto: string): any {
+    console.log(texto)
+    return this.appService.palabra(texto);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,21 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'HOLA MUNDO! de nuevoo';
+  }
+
+  hola() {
+    return { mensaje: 'Hola a todos' }
+  }
+
+  palabra( texto: string ) {
+    // return { mensaje: 'su palabra ingresada es: ' + texto + ' hdfgft' }
+    return { mensaje: `su palabra ingresada es: ${texto} y ${this.suma(5,8)} :D` }
+  }
+
+  // a: numbr , b: number
+  suma(a: number, b : number  ) {
+    const c =a +b
+    return `la suma es: ${c}`
   }
 }
